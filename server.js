73b78171dbecbed0299218a0f4080ead48243101
Alisonjs0 <alisonserafim.tec@gmail.com>
2026-02-08 03:44:44 -0300

    aa

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const cors = require('cors');
-require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 
 // Middleware
 app.use(cors());
